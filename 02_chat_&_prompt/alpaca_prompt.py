92e20338204415e646076214cea78ab3f206c76e
Pradeepks01 <pradeepks7483@gmail.com>
2025-04-15 21:55:01 +0530

    day 2 practice

---
+++ 02_chat_&_prompt/alpaca_prompt.py
@@ -0,0 +1,16 @@
+##  alpaca prompt
+# instruction :
+# for the given numbers by user perform 
+# input what is 1+1
+# Response : 2
+
+
+# Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+# ### Instruction:
+# {instruction}
+
+# ### Input:
+# {input}
+
+# ### Response:
